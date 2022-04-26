0f1b5ba3d56608869861b67144870b423717a50f
khanh2004 <duykhanhhoang@hotmail.com>
2022-04-26 08:38:14 +0700

    sdfhf

---
+++ hiển thị ngày trong tuần.py
@@ -0,0 +1,18 @@
+while True:    
+    so_nhap_vao = int(input("nhập số trong khoảng từ 1 đến 7..."))
+    if so_nhap_vao == 1:
+        print("monday")
+    elif so_nhap_vao == 2:
+        print("tuesday")
+    elif so_nhap_vao == 3:
+        print("wednesday")
+    elif so_nhap_vao == 4:
+        print("thursday")
+    elif so_nhap_vao == 5:
+        print("friday")
+    elif so_nhap_vao == 6:
+        print("saturday")
+    elif so_nhap_vao == 7:
+        print("sunday")
+    else:
+        print("không hợp lệ!")
