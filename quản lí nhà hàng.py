a45c9f917b4614663107dca713eca9424f9f9cb8
khanh2004 <duykhanhhoang@hotmail.com>
2022-05-10 06:47:37 +0700

    hjhjgghjhjgjh

--- quản lí nhà hàng.py
+++ quản lí nhà hàng.py
@@ -3,9 +3,9 @@ from tkinter import ttk
 from tkinter import *
 from tkinter import messagebox
 
-ten_mon_can_xoa = ""
 danh_sach_mon_an = [["mì tôm","25000"], ["phở","35000"]]
 shop_management = Tk()
+ten_mon_can_xoa = [""]
 def open1():    
     giao_dien_hoa_don = Toplevel()
     hoa_don_label = Label(giao_dien_hoa_don, text="in ra hóa đơn")
@@ -20,12 +20,12 @@ def xoa_mon():
     ten_mon_can_xoa_label.grid(column=0, row=0)
     ten_mon_can_xoa_entry.grid(column=1, row=0)
     thong_bao_da_xoa_mon_an_button.grid(column=1,row=2)
+    
     ten_mon_can_xoa = ten_mon_can_xoa_entry.get()
-     
 def an_nut_xoa ():
-        global ten_mon_can_xoa   
-        for ten_mon_an in danh_sach_mon_an:
-            if ten_mon_can_xoa in ten_mon_an :
+        global ten_mon_can_xoa
+        for ten_mon_an in range(len(danh_sach_mon_an)):
+            if ten_mon_can_xoa[ten_mon_an -1 ] in danh_sach_mon_an :
                 messagebox.showwarning("thông báo xóa món","đã xóa món thành công")
             else:
                 messagebox.showwarning("thông báo xóa món","xóa món không thành công")
@@ -47,7 +47,7 @@ def open ():
         my_tree.insert(parent="", index=END, values=(danh_sach_mon_an[counting_variable]))
         my_tree.pack()
     xoa_mon_button = Button(giao_dien_chinh_sua,text="xoá món ăn",command=xoa_mon).pack() 
-print(ten_mon_can_xoa)   
+
      
 ten_app_label = Label(shop_management, text="APP QUAN LI NHA HANG").grid(row=0, column=0, columnspan=2)
 danh_sach_mon_button = Button(shop_management, text="chinh sua danh sach mon", padx=40, pady=50, borderwidth=5, command=open).grid(row=1, column=0)
